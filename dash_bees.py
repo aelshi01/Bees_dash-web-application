d5a0d26f49b98a2cac37bf273b118d4542785416
aelshi01 <56831995+aelshi01@users.noreply.github.com>
2020-11-16 20:01:16 +0000

    update
    Updates dataset to a csv file and imported dataset into panda dataframe

---
+++ dash_bees.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import plotly.express as px  # (version 4.7.0)
+import plotly.graph_objects as go
+
+import dash  # (version 1.12.0) pip install dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+# ------------------------------------------------------------------------------
+# Import and clean data (importing csv into pandas)
+df = pd.read_csv("intro_bees.csv")
+
+df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
+df.reset_index(inplace=True)
+print(df[:5])
+
